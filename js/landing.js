7cbd8329d6ca26643615c38b965d767c7132d9a1
Ben Bagley <ben@benbagley.co.uk>
2018-12-31 02:01:05 +0000

    changes and products features section

--- js/landing.js
+++ js/landing.js
@@ -29,4 +29,126 @@ $(document).ready(function() {
     $(this).addClass('active')
            .siblings('.active').removeClass('active');
   });
+
+  // product features
+
+  // highly interactive
+  $('.highly-interactive').mouseenter( function() {
+    $('.highly-interactive-image').show();
+    $('h2', this).css('color', '#15d4ef');
+    $('.main-image').hide();
+    $('.hover', this).show();
+  });
+
+  $('.highly-interactive').mouseleave( function() {
+    $('.hover', this).hide();
+    $('h2', this).css('color', '#1A2D45');
+    $('.highly-interactive-image').hide();
+    $('.main-image').show();
+  });
+
+  // operating system
+  $('.operating-system').mouseenter( function() {
+    $('.operating-system-image').show();
+    $('h2', this).css('color', '#15d4ef');
+    $('.main-image').hide();
+    $('.hover', this).show();
+  });
+
+  $('.operating-system').mouseleave( function() {
+    $('.hover', this).hide();
+    $('h2', this).css('color', '#1A2D45');
+    $('.operating-system-image').hide();
+    $('.main-image').show();
+  });
+
+  // batteries
+  $('.batteries').mouseenter( function() {
+    $('.batteries-image').show();
+    $('h2', this).css('color', '#15d4ef');
+    $('.main-image').hide();
+    $('.hover', this).show();
+  });
+
+  $('.batteries').mouseleave( function() {
+    $('.hover', this).hide();
+    $('h2', this).css('color', '#1A2D45');
+    $('.batteries-image').hide();
+    $('.main-image').show();
+  });
+
+  // hypoallergenic
+  $('.hypoallergenic').mouseenter( function() {
+    $('.hypoallergenic-image').show();
+    $('h2', this).css('color', '#15d4ef');
+    $('.main-image').hide();
+    $('.hover', this).show();
+  });
+
+  $('.hypoallergenic').mouseleave( function() {
+    $('.hover', this).hide();
+    $('h2', this).css('color', '#1A2D45');
+    $('.hypoallergenic-image').hide();
+    $('.main-image').show();
+  });
+
+  // genuine personality
+  $('.genuine-personality').mouseenter( function() {
+    $('.genuine-personality-image').show();
+    $('h2', this).css('color', '#15d4ef');
+    $('.main-image').hide();
+    $('.hover', this).show();
+  });
+
+  $('.genuine-personality').mouseleave( function() {
+    $('.hover', this).hide();
+    $('h2', this).css('color', '#1A2D45');
+    $('.genuine-personality-image').hide();
+    $('.main-image').show();
+  });
+
+  // size
+  $('.size').mouseenter( function() {
+    $('.size-image').show();
+    $('h2', this).css('color', '#15d4ef');
+    $('.main-image').hide();
+    $('.hover', this).show();
+  });
+
+  $('.size').mouseleave( function() {
+    $('.hover', this).hide();
+    $('h2', this).css('color', '#1A2D45');
+    $('.size-image').hide();
+    $('.main-image').show();
+  });
+
+  // soft and safe
+  $('.soft-and-safe').mouseenter( function() {
+    $('.soft-and-safe-image').show();
+    $('h2', this).css('color', '#15d4ef');
+    $('.main-image').hide();
+    $('.hover', this).show();
+  });
+
+  $('.soft-and-safe').mouseleave( function() {
+    $('.hover', this).hide();
+    $('h2', this).css('color', '#1A2D45');
+    $('.soft-and-safe-image').hide();
+    $('.main-image').show();
+  });
+
+  // expanding app
+  $('.expanding-app').mouseenter( function() {
+    $('.expanding-app-image').show();
+    $('h2', this).css('color', '#15d4ef');
+    $('.main-image').hide();
+    $('.hover', this).show();
+  });
+
+  $('.expanding-app').mouseleave( function() {
+    $('.hover', this).hide();
+    $('h2', this).css('color', '#1A2D45');
+    $('.expanding-app-image').hide();
+    $('.main-image').show();
+  });
 });
